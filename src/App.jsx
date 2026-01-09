577883c878dc2354062c4313b50815ca36654228
“Rafly <cyberschoolindo@gmail.com>
2026-01-09 15:55:37 +0700

    Header bawah

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,10 @@ function App() {
 		<>
 			<nav></nav>
 
-			<header>
-				<div className="container bg-[#121212] text-[#edede4] m-auto max-w-full pt-5 pb-5">
+			<header className="bg-[#121212] text-[#edede4]">
+				<div className="container m-auto max-w-full pt-5 pb-5">
 					<div className="grid grid-cols-2 gap-4 py-10 px-16">
-						<div className="col-start-1 col-end-3">Frontend Developer</div>
-						<div className="col-span-2 col-end-7 text-right"></div>
+						<div className="col-start-1 col-end-3">Personal Portofolio</div>
 					</div>
 
 					<div className="flex flex-col items-center gap-4">
@@ -35,12 +34,33 @@ function App() {
 						</div>
 					</div>
 
-					<div className="grid grid-cols-2 gap-4 py-10 px-16">
+					<div className="grid grid-cols-2 gap-4 pt-10 px-16">
 						<div className="col-start-1 col-end-3">
-							Muhammad Rafly Maulidan Nico Tenges
+							<span className="font-bold text-2xl">////////</span>
 						</div>
-						<div className="col-span-2 col-end-7 text-right">
-							raflymaulidan019@gmail.com
+					</div>
+				</div>
+
+				<div className="content -mt-25">
+					<div className="grid grid-cols-4 gap-4 ">
+						<div className="col-start-1 col-end-4 text-left align-middle items-center object-center m-auto max-w-xl pt-20">
+							<h2 className="text-[45px] font-bold text-base/10">
+								MUHAMMAD RAFLY MAULIDAN NICO TENGES
+							</h2>
+							<br />
+							<p className="text-sm opacity-90">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+								qui saepe pariatur esse repellendus dolore magni velit fuga
+								consequuntur sed officiis aut illum facere ipsam tenetur
+								blanditiis quam, harum doloribus!
+							</p>
+						</div>
+						<div className="col-span-3 col-end-7 text-right">
+							<img
+								src="src/assets/img/image-me.png"
+								className="h-140"
+								alt="image-me"
+							/>
 						</div>
 					</div>
 				</div>
