2729c63007b033bbc66df1235e665c5aaf24ea4b
“Rafly <cyberschoolindo@gmail.com>
2026-04-30 23:20:52 +0700

    Responsif bagian about section

--- src/App.jsx
+++ src/App.jsx
@@ -66,33 +66,45 @@ function App() {
 					</div>
 
 					<div className="about" id="about">
-						<div className="md:flex block gap-2 py-10 max-w-7xl m-auto md:px-20 px-5">
-							<div className="m-auto w-2xl">
-								<h1 className="md:text-[28px] text-[24px] font-bold">
+						<div className="flex flex-col md:flex-row gap-10 py-10 max-w-7xl m-auto md:px-20 px-5 items-center">
+							<div className="w-full md:w-1/2 order-2 md:order-1">
+								<h1 className="md:text-[28px] text-[24px] font-bold md:text-left text-center">
 									Hi <span className="wave">👋</span>
 								</h1>
-								<h2 className="md:text-[28px] text-[14px] font-bold md:max-w-3xl max-w-sm">
+
+								<h2 className="md:text-[28px] text-[18px] font-bold md:max-w-3xl max-w-full leading-snug md:text-left text-center">
 									I am,{" "}
-									<span className="text-[#edede4] md:text-[30px] text-[20px] text-shadow-md text-shadow-white/50">
-										Muhammad Rafly Maulidan Nico Tenges
+									<span className="text-[#edede4] md:text-[30px] text-[18px] text-shadow-md text-shadow-white/50">
+										MUHAMMAD RAFLY MAULIDAN NICO TENGES
 									</span>
 								</h2>
-								<br /> <hr className="w-15 py-1" />
-								<p className="md:text-[14px] text-[12px] md:max-w-md max-w-xs leading-relaxed text-justify font-[Montserrat]">
+
+								<br />
+								<hr className="md:w-16 w-40 py-1 m-auto md:m-0" />
+
+								<p className="md:text-[14px] text-[12px] md:max-w-md max-w-full leading-relaxed md:text-justify text-center font-[Montserrat] mt-3">
 									I'm particularly interested in front-end development,
 									specifically building responsive, clean, and user-friendly
 									website interfaces across devices.
 								</p>
+
 								<br />
-								<a href={linkCv} target="_blank" rel="noopener noreferrer">
-									<button className="btn">
-										Download CV <i className="fa fa-solid fa-file"></i>
-									</button>
-								</a>
+
+								<div className="text-center md:text-left">
+									<a href={linkCv} target="_blank" rel="noopener noreferrer">
+										<button className="btn">
+											Download CV <i className="fa fa-solid fa-file"></i>
+										</button>
+									</a>
+								</div>
 							</div>
 
-							<div className="col-end-4 opacity-75">
-								<img src={Avatar} className="h-120" alt="image-me" />
+							<div className="flex justify-center md:justify-end items-center w-full md:w-1/2 order-1 md:order-2 -mb-6 md:mb-0">
+								<img
+									src={Avatar}
+									alt="image-me"
+									className="w-50 sm:w-62.5 md:w-87.5 lg:w-105 object-contain opacity-80 drop-shadow-[0_10px_30px_rgba(255,255,255,0.1)]"
+								/>
 							</div>
 						</div>
 					</div>
