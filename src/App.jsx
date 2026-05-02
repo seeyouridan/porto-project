7431eecb63b10590cca3ffe721b2736db837f5b7
“Rafly <cyberschoolindo@gmail.com>
2026-05-02 11:37:49 +0700

    Responsif teks bagian footer

--- src/App.jsx
+++ src/App.jsx
@@ -169,8 +169,8 @@ function App() {
 
 			<footer className="footer sm:footer-horizontal footer-center p-4">
 				<aside>
-					<div className="text-[#edede4] opacity-35 font-xs font-[Montserrat] text-shadow-md text-shadow-white/50">
-						<p>
+					<div className="text-[#edede4] opacity-35 font-xs font-[Montserrat] text-shadow-md text-shadow-white/50 flex">
+						<p className="max-w-xl md:w-auto w-55">
 							Copyright © {new Date().getFullYear()} seeyouridan. All rights reserved.
 						</p>
 					</div>
