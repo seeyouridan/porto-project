a557ddf7e06b8de107bf40506c537a4338281647
“Rafly <cyberschoolindo@gmail.com>
2026-05-14 00:05:44 +0700

    Desain icon dan hover pada section

--- src/components/layout/section/ContactSection.jsx
+++ src/components/layout/section/ContactSection.jsx
@@ -1,16 +1,31 @@
 function ContactSection() {
 	return (
 		<>
-			<div className="contact py-10 max-w-2xl mx-auto px-5" id="contact">
+			<div
+				className="contact py-10 md:max-w-xl max-w-87.5 mx-auto px-5"
+				id="contact"
+			>
 				<h2 className="text-center text-4xl font-[Montserrat] font-extrabold mb-12 text-shadow-md text-shadow-white/50">
-					CONTACT
+					CONNECT WITH ME
 				</h2>
-				<p className="text-center">
-					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta
-					consectetur delectus labore quia vitae facere nemo non iusto eveniet.
-					Culpa ipsum temporibus fuga? Totam repellendus minima repellat?
-					Officiis, repellendus animi!
-				</p>
+				<div className="flex justify-center font-[Montserrat]">
+					<div className="px-2">
+						<a
+							href="https://instagram.com/raflyidan_"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="group inline-flex items-center justify-center overflow-hidden rounded-full border text-[#f6f6ee] hover:bg-[#f6f6ee] hover:text-[#121212] transition-all duration-500 w-12 hover:w-52 px-3 py-3"
+						>
+							<div className="flex items-center justify-center min-w-max">
+								<i className="fa-brands fa-instagram text-xl shrink-0"></i>
+
+								<span className="max-w-0 overflow-hidden whitespace-nowrap opacity-0 transition-all duration-500 group-hover:max-w-xs group-hover:opacity-100">
+									<span className="ml-3">@raflyidan_</span>
+								</span>
+							</div>
+						</a>
+					</div>
+				</div>
 			</div>
 		</>
 	);
