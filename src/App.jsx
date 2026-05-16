629fda8b697ffaca01b744668b5f961c5ad87f5e
“Rafly <cyberschoolindo@gmail.com>
2026-05-16 23:51:53 +0700

    Membuat kustom komponen untuk scroll to top untuk page project

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
 import FieldWorkPractice from "@/pages/project-1/FieldWorkPractice";
 import DiscordBot from "@/pages/project-2/DiscordBot";
-import NotFound from "./pages/404";
+import NotFound from "@/pages/404";
+
+import useScrollToTop from "@/hooks/useScrollToTop";
 
 function App() {
+	useScrollToTop();
+
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
