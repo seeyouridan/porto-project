4f2def783ef204eade2b2820b937408fe03f67b6
“Rafly <cyberschoolindo@gmail.com>
2026-05-16 23:52:25 +0700

    fix: nav scroll project

--- src/components/layout/section/ProjectSection.jsx
+++ src/components/layout/section/ProjectSection.jsx
@@ -3,12 +3,14 @@ import CardEffect from "@/components/ui/CardEffect";
 function ProjectSection() {
 	return (
 		<>
-			<div className="project py-10 max-w-6xl m-auto" id="project">
-				<h2 className="text-center text-4xl font-[Montserrat] font-extrabold mb-12 text-shadow-md text-shadow-white/50">
-					PROJECTS
-				</h2>
-				<div className="flex flex-wrap justify-center gap-10">
-					<CardEffect />
+			<div className="project pt-40 pb-10" id="project">
+				<div className="max-w-6xl m-auto">
+					<h2 className="text-center text-4xl font-[Montserrat] font-extrabold mb-12 text-shadow-md text-shadow-white/50">
+						PROJECTS
+					</h2>
+					<div className="flex flex-wrap justify-center gap-10">
+						<CardEffect />
+					</div>
 				</div>
 			</div>
 		</>
