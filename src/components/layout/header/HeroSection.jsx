ce7b0be801cece80533ae898d380f1030960ad92
“Rafly <cyberschoolindo@gmail.com>
2026-05-19 18:39:57 +0700

    fit: responsive image & text

--- src/components/layout/header/HeroSection.jsx
+++ src/components/layout/header/HeroSection.jsx
@@ -5,7 +5,7 @@ function HeroSection() {
 		<>
 			<div className="hero m-auto max-w-full py-10">
 				<div className="gap-4 max-w-full relative w-full md:w-auto">
-					<div className="md:h-72 md:w-6xl w-full h-48 overflow-hidden md:rounded-sm">
+					<div className="md:h-72 lg:w-6xl md:w-4xl w-full h-48 overflow-hidden md:rounded-sm">
 						<img
 							src={HeaderImg}
 							className="h-full w-full object-cover object-[center_48%] md:scale-125 scale-200"
@@ -13,13 +13,13 @@ function HeroSection() {
 						/>
 					</div>
 
-					<div className="absolute left-1/2 -translate-x-1/2 -translate-y-30 hidden md:block">
-						<h1 className="text-[140px] text-center w-full opacity-90 font-bold">
+					<div className="absolute left-1/2 -translate-x-1/2 lg:-translate-y-30 md:-translate-y-20 hidden md:hidden lg:block">
+						<h1 className="lg:text-[140px] md:text-[100px] text-center w-full opacity-90 font-bold">
 							INTRODUCTION
 						</h1>
 					</div>
 
-					<div className="absolute left-1/2 -translate-x-1/2 -translate-y-20 md:hidden">
+					<div className="absolute left-1/2 -translate-x-1/2 -translate-y-20 md:block lg:hidden">
 						<h1 className="text-[105px] text-center w-full opacity-90 font-extrabold">
 							INTRO
 						</h1>
--- src/components/layout/section/ProjectSection.jsx
+++ src/components/layout/section/ProjectSection.jsx
@@ -8,7 +8,7 @@ function ProjectSection() {
 					<h2 className="text-center text-4xl font-[Montserrat] font-extrabold mb-12 text-shadow-md text-shadow-white/50">
 						PROJECTS
 					</h2>
-					<div className="flex flex-wrap justify-center gap-10">
+					<div className="flex flex-wrap justify-center md:gap-10 gap-5">
 						<CardEffect />
 					</div>
 				</div>
